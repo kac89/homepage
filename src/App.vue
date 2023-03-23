1a1d4a796ab359eaf71fc73c7dfe8c4b19dd2124
Kacper <admin@devilteam.pl>
2023-03-23 12:29:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/kacper.jpg" width="155" height="155" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Kacper Rybczyński" />
+    </div>
+  </header>
+
+  <main>
+
+    <p>First rule of business, protect your investment.</p>
+    <p>– Etiquette of the Banker, 1775</p>
+    <br>
+    <p>"The only way to get smarter is by playing a smarter opponent."</p>
+    <p>—Fundamentals of Chess 1883 </p>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <p>IT Security Expert</p><br>
+    <h3>
+      My projects<br>
+      <a href="https://vulnrepo.com/home" target="_blank" rel="noopener">VULNRΞPO</a>, 
+      <a href="https://cifrex.org/" target="_blank" rel="noopener">cIFrex</a>, 
+      <a href="https://devilteam.pl/" target="_blank" rel="noopener">DEVIL TEAM</a><br> and many 
+      <a href="https://github.com/kac89?tab=repositories" target="_blank" rel="noopener">others</a>.
+
+    </h3><br>
+    <h3>
+      Research<br>
+      <a href="https://www.exploit-db.com/?author=601" target="_blank" rel="noopener">Exploit Database</a>, 
+      <a href="http://0day.today/author/165" target="_blank" rel="noopener">My milw0rm</a>
+
+    </h3><br>
+    <h3>
+      Contact<br>
+      <a href="https://twitter.com/kacperybczynski" target="_blank" rel="noopener">@kacperybczynski</a><br>
+      <a href="https://github.com/kac89" target="_blank" rel="noopener">@kac89</a>
+    </h3>
+  </div>
+  <br><br>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings p,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings p,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
