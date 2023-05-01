0b7efd08863f77d2d763bd7912b8344d6287c7dc
Kacper <admin@devilteam.pl>
2023-05-01 21:43:51 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,5 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import { reactive, onMounted } from 'vue'
-
-const data = reactive({
-    time: ''
-});
-
-const getTravelofMyLife = () => {
-
-  const lightSpeed = 299792458; // prędkość światła w próżni w metrach na sekundę
-  const startTime = new Date("1989-01-06"); // czas początkowy w milisekundach       
-  const secondsInYear = 365.25 * 24 * 60 * 60; // liczba sekund w roku (uwzględniając rok przestępny)
-
-  let distanceMeters = 0;
-  // funkcja aktualizująca dystans na podstawie upływającego czasu
-
-  const currentTime = Date.now(); // aktualny czas w milisekundach
-  const elapsedTimeSeconds = (currentTime - startTime) / 1000; // upłynięty czas w sekundach
-  distanceMeters = lightSpeed * elapsedTimeSeconds;
-
-
-  // obliczamy dystans w kilometrach
-  const distanceKilometers = distanceMeters / 1000;
-  const yearsp = elapsedTimeSeconds / 31536000;
-
-  // wyświetlamy wynik w konsoli
-  //console.log(`Dystans pokonany przez foton w ciągu ${yearsp.toFixed(2)} lat to ${distanceKilometers.toFixed(2)} kilometrów.`);
-  //data.time = yearsp.toFixed(2) + ":" + distanceKilometers.toFixed(2);
-
-  data.time = distanceKilometers.toFixed(0);
-}
-
-onMounted(() => {
-    setInterval(getTravelofMyLife, 1000);
-  });
-
 </script>
 
 <template>
@@ -59,9 +24,7 @@ onMounted(() => {
           <br>
         </div>
         <br>
-        <div style="display:grid; place-items: center;padding-right: 25px;">
-          <h2 class="green" v-html="data.time"></h2>
-        </div>
+
         
     </div>
   </main>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { reactive, onMounted } from 'vue'
 
 defineProps({
   msg: {
@@ -7,12 +8,48 @@ defineProps({
   }
 });
 
+const data = reactive({
+    time: '0'
+});
+
+const getTravelofMyLife = () => {
+
+  const lightSpeed = 299792458; // prędkość światła w próżni w metrach na sekundę
+  const startTime = new Date("1989-01-06"); // czas początkowy w milisekundach       
+  const secondsInYear = 365.25 * 24 * 60 * 60; // liczba sekund w roku (uwzględniając rok przestępny)
+
+  let distanceMeters = 0;
+  // funkcja aktualizująca dystans na podstawie upływającego czasu
+
+  const currentTime = Date.now(); // aktualny czas w milisekundach
+  const elapsedTimeSeconds = (currentTime - startTime) / 1000; // upłynięty czas w sekundach
+  distanceMeters = lightSpeed * elapsedTimeSeconds;
+
+
+  // obliczamy dystans w kilometrach
+  const distanceKilometers = distanceMeters / 1000;
+  const yearsp = elapsedTimeSeconds / 31536000;
+
+  // wyświetlamy wynik w konsoli
+  //console.log(`Dystans pokonany przez foton w ciągu ${yearsp.toFixed(2)} lat to ${distanceKilometers.toFixed(2)} kilometrów.`);
+  //data.time = yearsp.toFixed(2) + ":" + distanceKilometers.toFixed(2);
+
+  data.time = distanceKilometers.toFixed(0);
+}
+
+onMounted(() => {
+    setInterval(getTravelofMyLife, 1000);
+  });
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <p>IT Security Expert</p><br>
+
+    <h3>My journey</h3>
+    <p class="green"><h3>{{ data.time }}</h3></p><br>
+
     <h3>
       My projects<br>
       <a href="https://vulnrepo.com/home" target="_blank" rel="noopener">VULNRΞPO</a>, 
